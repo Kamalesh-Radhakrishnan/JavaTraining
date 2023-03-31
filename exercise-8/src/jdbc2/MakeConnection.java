37f9e318fb1aa0a58ff26ad02d31980eb223bc62
Kamalesh-Radhakrishnan <125634379+Kamalesh-Radhakrishnan@users.noreply.github.com>
2023-03-31 20:01:06 +0530

    Update MakeConnection.java

--- exercise-8/src/jdbc2/MakeConnection.java
+++ exercise-8/src/jdbc2/MakeConnection.java
@@ -5,17 +5,23 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/**
+ * @author Kamalesh
+ *
+ */
 class MakeConnection {
-	Connection con; Statement stmt; ResultSet rs; MakeConnection() {
-		try{
+	Statement stmt;
+	ResultSet rs; 
+	private final String TABLE = "employees";
+	
+	MakeConnection() {
+		try(Connection con= DriverManager.getConnection("jdbc:mysql://localhost/practicedb","root","kamalesh2202@cdw");){
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con= DriverManager.getConnection("jdbc:mysql://localhost/practicedb","root","kamalesh2202@cdw");
 			stmt = con.createStatement();
-			int i=stmt.executeUpdate("create table employee(empno integer,ename varchar(20),deptno integer)");
+			int i=stmt.executeUpdate("create table "+ TABLE + "(empno integer,ename varchar(20),deptno integer)");
 			System.out.println("Result status: " + i);
-			con.close();
 		} catch(Exception e) { 
-			System.out.println(e);
+			e.printStackTrace();
 		}
 	} 
 }
